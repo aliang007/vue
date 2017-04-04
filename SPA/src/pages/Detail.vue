a4df99eebbadef4e0ec56e4c0927b59308b09b87
aliang007 <yesaliang@gmail.com>
2017-04-04 23:36:11 +0800

    购物车+SPA+商城系统

---
+++ SPA/src/pages/Detail.vue
@@ -0,0 +1,27 @@
+
+<!-- Detail.vue -->
+<template>
+    <div class="detail">
+      <div style="line-height:120px;">
+        {{ $route.params.color }}
+        <h1>我是详情页</h1><h1>我是详情页</h1><h1>我是详情页</h1>
+      </div>
+      <img src="http://p.lailaihui.com/LineBigPhoto/4561/3.jpg" alt="">
+    </div>
+</template>
+
+<style scoped>
+    .detail {
+      line-height: 30px;
+    }
+</style>
+
+<script>
+
+    import Hd from '../components/Header'
+    export default {
+        components: {
+            Hd
+        },
+    }
+</script>
---
+++ cart/README.md
@@ -0,0 +1,14 @@
+### 使用Vue2.0实现购物车和地址选配功能
+
+
+Vue2.0 + Vue-resource + ES6
+
+
+
+    |-- css                                 // css
+    |-- data                                // 假数据模拟
+    |-- img                                 // 图片
+    |-- .gitignore                          // push忽略文件
+    |-- index.html                          // 入口html页面
+    |-- address.html                        // 添加收货地址页面
+    |-- README.md                           // 简介
