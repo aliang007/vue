84369665db696c1752cd1600ddf86dc050c9a851
aliang007 <yesaliang@gmail.com>
2017-04-06 23:04:26 +0800

    slider幻灯片

--- shop/src/pages/index.vue
+++ shop/src/pages/index.vue
@@ -25,7 +25,7 @@
       </div>
     </div>
     <div class="index-right">
-      <slide-show :slides="slides" :inv="invTime"></slide-show>
+      <slider :sliders="sliders" :inv="invTime"></slider>
       <div class="index-board-list">
         <div
         class="index-board-item"
@@ -45,7 +45,11 @@
 </template>
 
 <script>
+  import slider from '../components/slider'
   export default{
+    components:{
+      slider
+    },
     created(){
 
       this.axios.get('/api/getNewsList')
@@ -61,6 +65,28 @@
     },
     data(){
       return {
+        sliders: [
+          {
+            src: require('../assets/slideShow/pic1.jpg'),
+            title: 'xxx1',
+            href: 'detail/analysis'
+          },
+          {
+            src: require('../assets/slideShow/pic2.jpg'),
+            title: 'xxx2',
+            href: 'detail/count'
+          },
+          {
+            src: require('../assets/slideShow/pic3.jpg'),
+            title: 'xxx3',
+            href: 'http://xxx.xxx.com'
+          },
+          {
+            src: require('../assets/slideShow/pic4.jpg'),
+            title: 'xxx4',
+            href: 'detail/forecast'
+          }
+        ],
         boardList: [
           {
             title: '开放产品',
